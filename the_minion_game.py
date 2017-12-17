a5dbe08328f726475ee011462176adf3bc121d95
Ekaterina Chernova <katyafervent@yandex.ru>
2017-12-18 00:27:45 +0300

    WORKS

--- the_minion_game.py
+++ the_minion_game.py
@@ -15,51 +15,102 @@
 # String  = BANANA
 # Kevin's vowel beginning word = ANA
 # Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
-import re
-
-VOWELS = "AEIOU"
-PLAYER1 = 'Kevin'
-PLAYER2 = 'Stuart'
-
-input_word = input()
-counted = set()
-length = len(input_word)
-result = {PLAYER1: 0, PLAYER2: 0}
 
 
-def count_substrings(substring, main_str=input_word):
-    # substring_re = '(?=({0}))'.format(re.escape(substring))
-    # return len(re.findall(substring_re, main_str))
-    start = 0
-    while True:
-        start = main_str.find(substring, start)
-        if start == -1:
-            return
-        yield start
-        start += 1
+# Using KMP algorithm to find substrings in a string
+#
+# VOWELS = "AEIOU"
+# PLAYER1 = 'Kevin'
+# PLAYER2 = 'Stuart'
+#
+# input_word = input()
+# counted = set()
+# length = len(input_word)
+# result = {PLAYER1: 0, PLAYER2: 0}
 
-for i, letter in enumerate(input_word):
-    if letter in VOWELS:
-        player = PLAYER1
-    else:
-        player = PLAYER2
+"BANANABANAN"
+# B
+# BA
+# BAN
+# BANA
 
-    for j in range(length-i):
-        score = 0
-        subst = input_word[i:j+i+1]
-        if subst in counted:
-            continue
-        # score_gen = count_substrings(subst)
-        for s in count_substrings(subst):
-            score += 1
 
-        counted.add(subst)
-        result[player] += score
-    counted.add(letter)
 
-if result[PLAYER1] > result[PLAYER2]:
-    print(PLAYER1, result[PLAYER1])
-elif result[PLAYER1] < result[PLAYER2]:
-    print(PLAYER2, result[PLAYER2])
-else:
-    print('Draw')
+def get_prefix_table(pattern):
+    table = [0]
+    pattern_length = len(pattern)
+    for i in range(1, pattern_length):
+        j = 0
+        while pattern[i] == pattern[j] and j < pattern_length:
+            j += 1
+        table.append(j)
+
+    return table
+
+def substring_index(needle, haystack):
+    prefix_table = get_prefix_table(needle)
+    haystack_length = len(haystack)
+    i = 0
+    while i < haystack_length:
+        j = 0
+        while haystack[i+j] == needle[j]:
+            j += 1
+            if len(needle) == j:
+                print("FIND {0}".format(i))
+                # return i
+                break
+                yield i
+
+        prefix_length = prefix_table[j-1]
+        if prefix_length > 0:
+            i += prefix_length
+            j += prefix_length
+        else:
+            i += 1
+        if i + j > haystack_length:
+            break
+    return -1
+
+for i in substring_index("ANAN", "BANANAN"):
+# for i in substring_index("YBYBO", "YBOYYBYBOXYBYBO"):
+    print(i)
+
+#
+# def count_substrings(substring, main_str=input_word):
+#     # substring_re = '(?=({0}))'.format(re.escape(substring))
+#     # return len(re.findall(substring_re, main_str))
+#     start = 0
+#     while True:
+#         start = main_str.find(substring, start)
+#         if start == -1:
+#             return
+#         yield start
+#         start += 1
+#
+# for i, letter in enumerate(input_word):
+#     if letter in VOWELS:
+#         player = PLAYER1
+#     else:
+#         player = PLAYER2
+#
+#     for j in range(length-i):
+#         score = 0
+#         subst = input_word[i:j+i+1]
+#         if subst in counted:
+#             continue
+#         # score_gen = count_substrings(subst)
+#         for s in count_substrings(subst):
+#             score += 1
+#
+#         counted.add(subst)
+#         result[player] += score
+#     counted.add(letter)
+#
+# if result[PLAYER1] > result[PLAYER2]:
+#     print(PLAYER1, result[PLAYER1])
+# elif result[PLAYER1] < result[PLAYER2]:
+#     print(PLAYER2, result[PLAYER2])
+# else:
+#     print('Draw')
+
+
