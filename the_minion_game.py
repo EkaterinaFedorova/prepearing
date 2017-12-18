14049166e8b043e7a59a81a6d84035c93c698ef5
Ekaterina Chernova <katyafervent@yandex.ru>
2017-12-18 21:13:52 +0300

    Counted all posiable substrings. The minion game.

--- the_minion_game.py
+++ the_minion_game.py
@@ -23,37 +23,7 @@ VOWELS = "AEIOU"
 PLAYER1 = 'Kevin'
 PLAYER2 = 'Stuart'
 
-
-def get_prefix_table(pattern):
-    table = [0]
-    pattern_length = len(pattern)
-    for i in range(1, pattern_length):
-        j = 0
-        while j < pattern_length and j > 0:
-            if pattern[i] == pattern[j]:
-                j += 1
-        table.append(j)
-    return table
-
-
-def substring_index(needle, haystack, prefixes):
-    haystack_length = len(haystack)
-    j = 0
-    for i in range(haystack_length):
-        while j > 0 and needle[j] != haystack[i]:
-            j = prefix_table[j - 1]
-        if needle[j] == haystack[i]:
-            j += 1
-            if len(needle) == j:
-                yield i - (j - 1)
-                i = i - (j - 1) + 1
-                j = prefixes[j - 1]
-    return None
-
-
-# input_word = "EQQAEAOQYEQEYYOEEQQYAOEEAQEEOOEYAYOEYAYAEOQYAAYAOYYOQAAYEQAOOAQEAEYAOEEQYYEEAOAOAEQOEYOAOEYOOAAOQEOYEAYYOEAOAQEYYEOQEEEYAOOAYOOAQAEOYOYAEOYQEEEOOQOEAOAAQAOQEYOQEAEAEOOOOQOYQOEQQYEEEYEEOQYYYOEQOQEYAYQQOYEEOAEQOEEEEAAEEYAAQAAQAAYOEAQAQYEYYYEAOYOQEQOOEQOYAYAEEYQAYYQYYAEAYOEYEAOQQQOYYYOYYOYYQYAOEOAOAOYEAAOEOEAEYQAEAQOEOYEEAQOAOQEYOEQOAQQEEYOOAQQOOEYQAQOEEOOOAAQOQEYYOEOOQOOAEYEOOAEQYQOAEYYYAQAYOEYOEYYEEOEEOAYAEEQEQOAAAYAEYQQAYOYQQOAEAOQOOYAEEOAEQAQEEQYOOEEAEEAAOYQYQAOEQYOYEQEAAOYAQAQYEAQEQEEOQQQYEYOQ"
 input_word = "BAANANAS"
-counted = set()
 length = len(input_word)
 result = {PLAYER1: 0, PLAYER2: 0}
 
@@ -63,23 +33,8 @@ for i, letter in enumerate(input_word):
         player = PLAYER1
     else:
         player = PLAYER2
-    prefix_table = get_prefix_table(input_word)
-    for j in range(length-i):
-        score = 0
-        subst = input_word[i:j+i+1]
-        prefixes = prefix_table[i:j + i + 1]
-        if subst in counted:
-            continue
-        # score_gen = count_substrings(subst)
-        if j == 0:
-            score = input_word.count(subst)
-        else:
-            for s in substring_index(subst, input_word, prefixes):
-                score += 1
-        print(subst, score)
-        counted.add(subst)
-        result[player] += score
-    counted.add(letter)
+    score = length - i
+    result[player] += score
 
 if result[PLAYER1] > result[PLAYER2]:
     print(PLAYER1, result[PLAYER1])
